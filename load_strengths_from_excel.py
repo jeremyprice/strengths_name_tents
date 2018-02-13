48a9c71fde9e25feced0fad66f3ffac18b1cfc3b
Jeremy Price <jprice.rhit@gmail.com>
2018-02-12 19:11:32 -0600

    added processing for McCollum HEB

--- load_strengths_from_excel.py
+++ load_strengths_from_excel.py
@@ -8,9 +8,7 @@ def unwrap_strengths(unw):
     lines = unw.split('\n')
     return [u.split('.')[1].strip() for u in lines]
 
-def main(xl_fname, output_dir):
-    wb = openpyxl.load_workbook(xl_fname, read_only=True)
-    ws = wb.active
+def BOA_and_McCollum(ws, output_dir):
     for row in ws.iter_rows(min_row=3):
         mentor_name = row[0].value
         mentor_strengths = unwrap_strengths(row[1].value)
@@ -21,5 +19,25 @@ def main(xl_fname, output_dir):
         fname = '{}/{}.pdf'.format(output_dir, mentee_name)
         render_pdf.create_name_tent(fname, mentee_name, mentee_strengths)
 
+def McCollum_HEB_Student_Strengths(ws, output_dir):
+    for row in ws.iter_rows(min_row=2):
+        mentee_name = row[0].value
+        if mentee_name is None:
+            continue
+        mentee_strengths = [row[1].value, row[2].value, row[3].value]
+        fname = '{}/{}.pdf'.format(output_dir, mentee_name)
+        render_pdf.create_name_tent(fname, mentee_name, mentee_strengths)
+
+def main(xl_fname, output_dir, proc=None):
+    wb = openpyxl.load_workbook(xl_fname, read_only=True)
+    ws = wb.active
+    if proc == 'BOA and McCollum':
+        BOA_and_McCollum(ws, output_dir)
+    elif proc == 'McCollum HEB Student Strengths':
+        McCollum_HEB_Student_Strengths(ws, output_dir)
+    else:
+        print('Unknown processing: {!r}'.format(proc))
+
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    proc = sys.argv[1].split('/')[-1].split('.')[0]
+    main(sys.argv[1], sys.argv[2], proc)
