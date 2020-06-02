486afa8bcd825327598e7453158dd6cac033ab3d
Jeremy Price <jprice@alumni.rose-hulman.edu>
2020-06-01 21:35:04 -0500

    process entire ro file and gen mini output

--- load_strengths_from_excel.py
+++ load_strengths_from_excel.py
@@ -12,6 +12,7 @@ def get_name_talents(series):
     talents = (series['S1'], series['S2'], series['S3'], series['S4'], series['S5'])
     return (name, talents)
 
+
 def main(xlfname, output_dir, imgfname=None, mini=False):
     if mini:
         renderer = render_mini_pdf
@@ -32,14 +33,33 @@ def main(xlfname, output_dir, imgfname=None, mini=False):
         bottom_name, bottom_talents = get_name_talents(df.iloc[row_idx + 1])
         if np.nan in top_talents:
             print('Skipping {}: no Strengths'.format(top_name))
-            spares.append((bottom_name, bottom_talents))
+            if not np.nan in bottom_talents:
+                spares.append((bottom_name, bottom_talents))
+            else:
+                print('Skipping {}: no Strengths'.format(bottom_name))
             continue
         if np.nan in bottom_talents:
             print('Skipping {}: no Strengths'.format(bottom_name))
             spares.append((top_name, top_talents))
             continue
         renderer.create_name_tent(fname, top_name, top_talents, bottom_name, bottom_talents, imgfname)
-
+    #TODO: go through the spares and print them
+    if len(spares) % 2 == 1:
+        # odd
+        end = len(spares) - 1
+        odd = True
+    else:
+        end = len(spares)
+        odd = False
+    for row_idx in range(0, end, 2):
+        fname = '{:s}/spares-{:02d}.pdf'.format(output_dir, row_idx)
+        top_name, top_talents = spares[row_idx]
+        bottom_name, bottom_talents = spares[row_idx+1]
+        renderer.create_name_tent(fname, top_name, top_talents, bottom_name, bottom_talents, imgfname)
+    if odd:
+        fname = '{:s}/spares-{:02d}.pdf'.format(output_dir, len(spares)-1)
+        top_name, top_talents = spares[-1]
+        renderer.create_name_tent(fname, top_name, top_talents, None, None, imgfname)
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         print('Usage: {} <xlfname> <output_dir> <imgfname>'.format(sys.argv[0]))
--- render_mini_pdf.py
+++ render_mini_pdf.py
@@ -127,15 +127,17 @@ def print_quadrant(canvas, image, rotate, translate_w, translate_h, name, talent
     print_line(canvas)
     canvas.restoreState()
 
-def create_name_tent(fname, top_name, top_talents, bottom_name, bottom_talents, image=None):
+def create_name_tent(fname, top_name=None, top_talents=None, bottom_name=None, bottom_talents=None, image=None):
     canvas = create_pdf_canvas(fname)
     # print the fold and cut lines
     print_fold_cut_lines(canvas)
     # setup the rotate, translations, and info
-    print_quadrant(canvas, image, 90, PAGE_WIDTH/2, PAGE_HEIGHT/2, top_name, top_talents)
-    print_quadrant(canvas, image, 90, PAGE_WIDTH/2, 0, bottom_name, bottom_talents)
-    print_quadrant(canvas, image, 270, PAGE_WIDTH/2, PAGE_HEIGHT/2, bottom_name, bottom_talents)
-    print_quadrant(canvas, image, 270, PAGE_WIDTH/2, PAGE_HEIGHT, top_name, top_talents)
+    if not top_name is None and not top_talents is None:
+        print_quadrant(canvas, image, 90, PAGE_WIDTH/2, PAGE_HEIGHT/2, top_name, top_talents)
+        print_quadrant(canvas, image, 270, PAGE_WIDTH/2, PAGE_HEIGHT/2, bottom_name, bottom_talents)
+    if not bottom_name is None and not bottom_talents is None:
+        print_quadrant(canvas, image, 90, PAGE_WIDTH/2, 0, bottom_name, bottom_talents)
+        print_quadrant(canvas, image, 270, PAGE_WIDTH/2, PAGE_HEIGHT, top_name, top_talents)
 
     canvas.showPage()
     canvas.save()
